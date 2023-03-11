90d8d605a782ed6ae5c69de7a288a5b9a6a39f0a
Josh Israel <uno.braid@gmail.com>
2023-03-11 02:17:16 -0300

    feat(infra): Created a new Nuxt3 app and configured Its build options to work with the Nx monorepo.

---
+++ apps/my-app/nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  nitro: {
+    output: {
+      dir: '~~/../../dist/apps/my-app',
+    },
+  },
+  srcDir: 'src/',
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      title: 'my-app',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'my-app' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+});
---
+++ apps/my-app/src/app.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>my-app</div>
+</template>

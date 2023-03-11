e71ee539f89aacee3b5d919e23984c391bc61b58
Josh Israel <uno.braid@gmail.com>
2023-03-11 05:15:20 -0300

    feat(my-app): Installed the nuxt-icons module to enable the easy usage of custom svgs. Created a global.css file for global declarations.

--- apps/my-app/nuxt.config.ts
+++ apps/my-app/nuxt.config.ts
@@ -20,6 +20,7 @@ export default defineNuxtConfig({
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
   },
+  css: ['~/assets/css/global.css'],
   modules: [
     '@nuxtjs/robots',
     '@nuxtjs/tailwindcss',
@@ -52,5 +53,6 @@ export default defineNuxtConfig({
         },
       },
     ],
+    'nuxt-icons',
   ],
 });
--- apps/my-app/src/pages/index.vue
+++ apps/my-app/src/pages/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col">
+  <div class="flex items-center gap-2">
     <h1>my-app</h1>
+    <NuxtIcon name="logo" filled />
   </div>
 </template>
